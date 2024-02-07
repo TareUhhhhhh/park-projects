0023cc375c76fd1f6e7c691cfe9215a94928daea
Tara Haskamp <1721345@park.edu>
2024-02-07 11:03:04 -0600

    Remove unused javascript

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,36 +1,3 @@
-  // showSlides(slideIndex);
-
-  // // Next/previous controls
-  // function changeSlide(n) {
-  //   showSlides(slideIndex += n);
-  // }
-
-  // // Thumbnail image controls
-  // function currentSlide(n) {
-  //   showSlides(slideIndex = n);
-  // }
-
-  // function showSlides(n) {
-  //   var i;
-  //   var slides = document.getElementsByClassName("slide");
-  //   var dots = document.getElementsByClassName("dot");
-  //   if (n > slides.length) {slideIndex = 1}
-  //   if (n < 1) {slideIndex = slides.length}
-  //   for (i = 0; i < slides.length; i++) {
-  //       slides[i].style.display = "none";
-  //   }
-  //   for (i = 0; i < dots.length; i++) {
-  //       dots[i].className = dots[i].className.replace(" active", "");
-  //   }
-  //   slides[slideIndex-1].style.display = "block";
-  //   dots[slideIndex-1].className += " active";
-  // }
-
-  // // Automatic slideshow
-  // setInterval(function() {
-  //   changeSlide(1);
-  // }, 5000); // Change slide every 5 seconds
-
   var slideIndex = 1;
   showSlides(slideIndex);
 
