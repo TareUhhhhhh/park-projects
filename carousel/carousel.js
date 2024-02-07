ca960cf2ddcba2321c4efea1b1c5c6772f627ef9
Tara <92134159+TareUhhhhhh@users.noreply.github.com>
2024-02-07 10:39:15 -0600

    Add carousel project

---
+++ carousel/carousel.js
@@ -0,0 +1,82 @@
+  // showSlides(slideIndex);
+
+  // // Next/previous controls
+  // function changeSlide(n) {
+  //   showSlides(slideIndex += n);
+  // }
+
+  // // Thumbnail image controls
+  // function currentSlide(n) {
+  //   showSlides(slideIndex = n);
+  // }
+
+  // function showSlides(n) {
+  //   var i;
+  //   var slides = document.getElementsByClassName("slide");
+  //   var dots = document.getElementsByClassName("dot");
+  //   if (n > slides.length) {slideIndex = 1}
+  //   if (n < 1) {slideIndex = slides.length}
+  //   for (i = 0; i < slides.length; i++) {
+  //       slides[i].style.display = "none";
+  //   }
+  //   for (i = 0; i < dots.length; i++) {
+  //       dots[i].className = dots[i].className.replace(" active", "");
+  //   }
+  //   slides[slideIndex-1].style.display = "block";
+  //   dots[slideIndex-1].className += " active";
+  // }
+
+  // // Automatic slideshow
+  // setInterval(function() {
+  //   changeSlide(1);
+  // }, 5000); // Change slide every 5 seconds
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function changeSlide(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+  // Open the Modal
+  function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    var slides = document.getElementsByClassName("mySlides");
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  // Automatic slideshow
+  setInterval(function() {
+    changeSlide(1);
+  }, 5000); // Change slide every 5 seconds
